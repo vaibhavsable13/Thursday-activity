c30e06513b3d16598361af7c4ab33cb077c1b391
vaibhavsable13 <118442396+vaibhavsable13@users.noreply.github.com>
2022-11-21 05:03:36 -0800

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,28 @@
+package com.Question3;
+import java.util.Scanner;
+public class numberpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i, j, k = 1;
+
+		for (i = 1; i <= 5; i++) {
+
+		for (j = 1; j < i + 1; j++) {
+
+		System.out.print(k++ + " "); 
+
+		}
+
+		System.out.println();
+
+		}
+
+		}
+
+		}
+	}
+
+}
