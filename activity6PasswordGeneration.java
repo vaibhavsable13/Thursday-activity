80e7b95ccc662e62df8ea0a2df6309632b61bc3f
vaibhavsable13 <118442396+vaibhavsable13@users.noreply.github.com>
2022-12-22 06:08:23 -0800

    Add files via upload

---
+++ activity6PasswordGeneration.java
@@ -0,0 +1,29 @@
+//Author:vaibhav sable
+//password contain 8 characters one uppercase, one lowercase, one special character
+package com.question1;
+
+import java.util.*;
+import java.util.regex.*;
+
+	public class PasswordGeneration {
+		
+	        public static void main(String a[]){
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Generate your Security Code:");
+	        String code = sc.next();
+	        String regex = "[A-Za-z0-9@*#]{8}";
+	        Pattern p = Pattern.compile(regex);
+	        Matcher m = p.matcher(code);
+	        boolean matchFound = m.find();
+	        if(matchFound)
+	        {
+	        System.out.println("Security Code Generated Successfully");
+	        }
+	    
+	        else
+	        {
+	        System.out.println("Invalid Security Code...... Try Again!");
+	        }
+	    }
+	}
+
